93ea4a99afa6edae4b5f4d528fc355d61dea08cd
Nelly-tech256 <ndalahwanelly@gmail.com>
2022-11-03 04:09:32 -0700

    C-recusion

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,14 +2,18 @@
 
 /**
  * factorial ->function of a given factorial
- *@n: number
- *Return: an interger
+ * @n: number
+ * Return: an interger
  **/
 int factorial(int n);
 {
 	if (n < 0)
+	{
 		return (-1);
-	if (n <= 1)
+	}
+	if (n == 1)
+	{
 		return (1);
+	}
 	return (n * factorial(n - 1));
 }
