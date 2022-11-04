a42c7c96d8863342eacb6da30ab4b1eebe85ba5e
Nelly-tech256 <ndalahwanelly@gmail.com>
2022-11-04 07:33:38 -0700

    0x0A. C - argc, argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - this is a function to print its name
+ * main - a program that prints its name, followed by a new line.
  * @argc : argc parameter
  * @argv : an array of a command listed
  * Return : 0 for success
