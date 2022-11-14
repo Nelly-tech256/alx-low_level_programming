28491aeb31057e2df87d78be603d54294ce89019
Nelly-tech256 <ndalahwanelly@gmail.com>
2022-11-14 06:36:33 -0800

    a function-like macro ABS(x) that computes the absolute value of a number x.

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /*
- * main - prints the name of the file it was compiled from,
+ * main - prints the name of the file of the program it was compiled from,
  *        followed by a new line.
- * Return : 0.
+ * Return: sucess 0.
  */
 int main(void)
 {
