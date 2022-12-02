e76bc84122de9e09fe2a589914f19fe597a7983f
Nelly-tech256 <ndalahwanelly@gmail.com>
2022-12-02 05:45:10 -0800

    Convert a binary digit to an unsigned int decimal.

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <string.h>
 /**
  * binary_to_uint - a function that converts
  *                a binary number to an unsigned int
@@ -12,20 +12,42 @@
  *
  */
 unsigned int binary_to_uint(const char *b)
+/**
+ * {
+ *	unsigned int num = 0;
+ *	int len = 0;
+ *
+ *	if (b[len] == '\0')
+ *		return (0);
+ *
+ *	while ((b[len] == '0' || (b[len] == '1')))
+ *	{
+ *		num <<= 1;
+ *		num += b[len] - '0';
+ *		len++;
+ *	}
+ *
+ *	return (num);
+ *
+ *  }
+ */
 {
-	unsigned int num = 0;
-	int len = 0;
+	unsigned int c, d;
 
-	if (b[len] == '\0')
+	if (!b)
 		return (0);
 
-	while ((b[len] == '0' || (b[len] == '1')))
+	c = 0;
+	for (d = 0; d < strlen(b); d++)
 	{
-		num <<= 1;
-		num += b[len] - '0';
-		len++;
+
+		/* shift c one bit to the left */
+		c <<= 1;
+		if (b[d] == '1')
+			/* set each on bit. */
+			c != 1;
+		else if (b[d] == '-' || b[d] != '0')
+			return (0);
 	}
-
-	return (num);
-
 }
+
